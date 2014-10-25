59f3e79b3077a4238c1300aec88d9e18b4360a56
jhauswald <jahausw@umich.edu>
2014-10-25 13:55:09 -0400

    local cuda

--- src/caffe/layers/local_layer.cu
+++ src/caffe/layers/local_layer.cu
@@ -8,79 +8,79 @@
 
 namespace caffe {
 
-/* template <typename Dtype> */
-/* __global__ void local_update1_gpu_kernel(const Dtype* data_A, const Dtype* data_B, */
-/*                                     Dtype* data_R, const int filter_num, */
-/*                                     const int location_num, const int output_num) { */
-/*   int total = filter_num * location_num * output_num; */
-/*   CUDA_KERNEL_LOOP(index, total) { */
-/*     int p = index % location_num; */
-/*     int n = (index / location_num) % filter_num; */
-/*     int q = (index / location_num) / filter_num; */
-/*     data_R[index] += data_A[q*location_num+p] * data_B[n*location_num+p]; */
-/*   } */
-/* } */
-/*  */
-/* template <typename Dtype> */
-/* void local_update1_gpu(const Dtype* data_A, const Dtype* data_B, */
-/*                        Dtype* data_R, const int filter_num, */
-/*                        const int location_num, const int output_num) { */
-/*   // data_A is output_num x location_num */
-/*   // data_B is filter_num x location_num */
-/*   // data_R is output_num x filter_num x location_num, the update performed is Rqnp += Aqp * Bnp */
-/*  */
-/*   // NOLINT_NEXT_LINE(whitespace/operators) */
-/*   local_update1_gpu_kernel<Dtype><<<CAFFE_GET_BLOCKS(filter_num * location_num * output_num), */
-/*                              CAFFE_CUDA_NUM_THREADS>>>(data_A, data_B, data_R, filter_num, location_num, output_num); */
-/*   CUDA_POST_KERNEL_CHECK; */
-/* } */
-/*  */
-/* // Explicit instantiation */
-/* template void local_update1_gpu<float>(const float* data_A, const float* data_B, */
-/*                                 float* data_R, const int filter_num, */
-/*                                 const int location_num, const int output_num); */
-/* template void local_update1_gpu<double>(const double* data_A, const double* data_B, */
-/*                                 double* data_R, const int filter_num, */
-/*                                 const int location_num, const int output_num); */
-/*  */
-/*  */
-/* template <typename Dtype> */
-/* __global__ void local_update2_gpu_kernel(const Dtype* data_A, const Dtype* data_B, */
-/*                                 Dtype* data_R, const int filter_num, */
-/*                                 const int location_num, const int output_num) { */
-/*   int total = filter_num * location_num; */
-/*   CUDA_KERNEL_LOOP(index, total) { */
-/*     int p = index % location_num; */
-/*     int n = (index / location_num); */
-/*     for (int q=0; q<output_num; q++) { */
-/*       data_R[index] += data_A[q*location_num+p] * data_B[(q*filter_num+n)*location_num+p]; */
-/*     } */
-/*   } */
-/* } */
-/*  */
-/* template <typename Dtype> */
-/* void local_update2_gpu(const Dtype* data_A, const Dtype* data_B, */
-/*                        Dtype* data_R, const int filter_num, */
-/*                        const int location_num, const int output_num) { */
-/*   // data_A is output_num x location_num */
-/*   // data_B is output_num x filter_num x location_num */
-/*   // data_R is filter_num x location_num, the update performed is Rnp += \sum_q(Aqp * Bqnp) */
-/*  */
-/*   // NOLINT_NEXT_LINE(whitespace/operators) */
-/*   local_update2_gpu_kernel<Dtype><<<CAFFE_GET_BLOCKS(filter_num * location_num), */
-/*                              CAFFE_CUDA_NUM_THREADS>>>(data_A, data_B, data_R, filter_num, location_num, output_num); */
-/*   CUDA_POST_KERNEL_CHECK; */
-/* } */
-/*  */
-/* // Explicit instantiation */
-/* template void local_update2_gpu<float>(const float* data_A, const float* data_B, */
-/*                        float* data_R, const int filter_num, */
-/*                        const int location_num, const int output_num); */
-/* template void local_update2_gpu<double>(const double* data_A, const double* data_B, */
-/*                        double* data_R, const int filter_num, */
-/*                        const int location_num, const int output_num); */
-/*  */
-/*  */
+template <typename Dtype>
+__global__ void local_update1_gpu_kernel(const Dtype* data_A, const Dtype* data_B,
+                                    Dtype* data_R, const int filter_num,
+                                    const int location_num, const int output_num) {
+  int total = filter_num * location_num * output_num;
+  CUDA_KERNEL_LOOP(index, total) {
+    int p = index % location_num;
+    int n = (index / location_num) % filter_num;
+    int q = (index / location_num) / filter_num;
+    data_R[index] += data_A[q*location_num+p] * data_B[n*location_num+p];
+  }
+}
+
+template <typename Dtype>
+void local_update1_gpu(const Dtype* data_A, const Dtype* data_B,
+                       Dtype* data_R, const int filter_num,
+                       const int location_num, const int output_num) {
+  // data_A is output_num x location_num
+  // data_B is filter_num x location_num
+  // data_R is output_num x filter_num x location_num, the update performed is Rqnp += Aqp * Bnp
+
+  // NOLINT_NEXT_LINE(whitespace/operators)
+  local_update1_gpu_kernel<Dtype><<<CAFFE_GET_BLOCKS(filter_num * location_num * output_num),
+                             CAFFE_CUDA_NUM_THREADS>>>(data_A, data_B, data_R, filter_num, location_num, output_num);
+  CUDA_POST_KERNEL_CHECK;
+}
+
+// Explicit instantiation
+template void local_update1_gpu<float>(const float* data_A, const float* data_B,
+                                float* data_R, const int filter_num,
+                                const int location_num, const int output_num);
+template void local_update1_gpu<double>(const double* data_A, const double* data_B,
+                                double* data_R, const int filter_num,
+                                const int location_num, const int output_num);
+
+
+template <typename Dtype>
+__global__ void local_update2_gpu_kernel(const Dtype* data_A, const Dtype* data_B,
+                                Dtype* data_R, const int filter_num,
+                                const int location_num, const int output_num) {
+  int total = filter_num * location_num;
+  CUDA_KERNEL_LOOP(index, total) {
+    int p = index % location_num;
+    int n = (index / location_num);
+    for (int q=0; q<output_num; q++) {
+      data_R[index] += data_A[q*location_num+p] * data_B[(q*filter_num+n)*location_num+p];
+    }
+  }
+}
+
+template <typename Dtype>
+void local_update2_gpu(const Dtype* data_A, const Dtype* data_B,
+                       Dtype* data_R, const int filter_num,
+                       const int location_num, const int output_num) {
+  // data_A is output_num x location_num
+  // data_B is output_num x filter_num x location_num
+  // data_R is filter_num x location_num, the update performed is Rnp += \sum_q(Aqp * Bqnp)
+
+  // NOLINT_NEXT_LINE(whitespace/operators)
+  local_update2_gpu_kernel<Dtype><<<CAFFE_GET_BLOCKS(filter_num * location_num),
+                             CAFFE_CUDA_NUM_THREADS>>>(data_A, data_B, data_R, filter_num, location_num, output_num);
+  CUDA_POST_KERNEL_CHECK;
+}
+
+// Explicit instantiation
+template void local_update2_gpu<float>(const float* data_A, const float* data_B,
+                       float* data_R, const int filter_num,
+                       const int location_num, const int output_num);
+template void local_update2_gpu<double>(const double* data_A, const double* data_B,
+                       double* data_R, const int filter_num,
+                       const int location_num, const int output_num);
+
+
 /// @brief refer to CPU forward -- the BLAS implementation is the same.
 template <typename Dtype>
 void LocalLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
@@ -160,11 +160,11 @@ void LocalLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
     im2col_gpu(bottom_data + (*bottom)[0]->offset(n), channels_, height_,
                width_, kernel_size_, kernel_size_, pad_, pad_, stride_, stride_, x_data);
 
-    /* local_update1_gpu(top_diff+top[0]->offset(n), x_data, weight_diff, K_, N_, M_); */
+    local_update1_gpu(top_diff+top[0]->offset(n), x_data, weight_diff, K_, N_, M_);
 
     if (propagate_down[0]) {
       CUDA_CHECK(cudaMemset(x_diff, 0, col_buffer_.count() * sizeof(Dtype)));
-      /* local_update2_gpu(top_diff+top[0]->offset(n), weight, x_diff, K_, N_, M_); */
+      local_update2_gpu(top_diff+top[0]->offset(n), weight, x_diff, K_, N_, M_);
 
       // col2im back to the data
       col2im_gpu(x_diff, channels_, height_, width_, kernel_size_, kernel_size_,
@@ -173,6 +173,6 @@ void LocalLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
   }
 }
 
-INSTANTIATE_LAYER_GPU_FUNCS(LocalLayer);
+INSTANTIATE_CLASS(LocalLayer);
 
 }  // namespace caffe
