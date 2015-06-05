48ca4578343a783f81a369f93a66fc22bc7c9025
jahausw <jahausw@umich.edu>
2015-06-05 01:22:19 -0400

    tw

--- get-libs.sh
+++ get-libs.sh
@@ -3,6 +3,7 @@ tar zxvf glog-0.3.3.tar.gz
 cd glog-0.3.3
 ./configure
 make && make install
+cd -
 
 # gflags
 wget https://github.com/schuhschuh/gflags/archive/v1.7.tar.gz
@@ -11,15 +12,14 @@ cd gflags-1.7/
 export CXXFLAGS="-fPIC" && cmake .. && make VERBOSE=1
 ./configure
 make && make install
+cd -
 
 # lmdb
 
 git clone https://gitorious.org/mdb/mdb.git
 cd mdb/libraries/liblmdb
 make && make install
+cd -
 
 apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev \
 libboost-all-dev libhdf5-serial-dev libopenblas-dev
-
-# open blas installs to a subdirectory
-sudo cp /usr/lib/openblas-base/* /usr/lib
--- make-and-install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-make -j 4
-make distribute
-sudo cp distribute/lib/libcaffe.so /usr/local/lib
-sudo cp -r distribute/include/caffe /usr/local/include
